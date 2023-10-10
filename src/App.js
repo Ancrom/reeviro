d5bcb4715ac35ca8fd8837ea0bb978d400be7661
Roman Antsupov <remon90@list.ru>
2023-10-10 19:02:45 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from "react";
-
 import "./styles/reset.css";
 import "./styles/AOS.css";
 import "./styles/base.css";
-
 import Footer from "./../src/components/footer/Footer";
 import BtnContactMe from "../src/components/btnContactMe/BtnContactMe";
 import MobileMenu from "../src/components/mobileMenu/MobileMenu";
-
 import Home from "./pages/Home";
 import Concept from "./pages/Concept";
 import Illustration from "./pages/Illustration";
@@ -19,81 +16,32 @@ import AboutMe from "./pages/AboutMe";
 function App() {
   const [menuActive, setMenuActive] = useState(false);
   const [lightboxActive, setLightboxActive] = useState(false);
+
+  const commonProps = {
+    navColor: "dark",
+    menuActive,
+    setMenuActive,
+    lightboxActive,
+    setLightboxActive,
+  };
+
   return (
     <div className="App">
       <Router>
         <main className="main">
           <Routes>
-            <Route
-              exact
-              path="/"
-              element={
-                <Home
-                  menuActive={menuActive}
-                  setMenuActive={setMenuActive}
-                  lightboxActive={lightboxActive}
-                  setLightboxActive={setLightboxActive}
-                />
-              }
-            />
-            <Route
-              path="/concept"
-              element={
-                <Concept
-                  navColor={"dark"}
-                  menuActive={menuActive}
-                  setMenuActive={setMenuActive}
-                  lightboxActive={lightboxActive}
-                  setLightboxActive={setLightboxActive}
-                />
-              }
-            />
+            <Route path="/" element={<Home {...commonProps} />} />
+            <Route path="/concept" element={<Concept {...commonProps} />} />
             <Route
               path="/illustration"
-              element={
-                <Illustration
-                  navColor={"dark"}
-                  menuActive={menuActive}
-                  setMenuActive={setMenuActive}
-                  lightboxActive={lightboxActive}
-                  setLightboxActive={setLightboxActive}
-                />
-              }
+              element={<Illustration {...commonProps} />}
             />
-            <Route
-              path="/project"
-              element={
-                <Project
-                  navColor={"dark"}
-                  menuActive={menuActive}
-                  setMenuActive={setMenuActive}
-                  lightboxActive={lightboxActive}
-                  setLightboxActive={setLightboxActive}
-                />
-              }
-            />
+            <Route path="/project" element={<Project {...commonProps} />} />
             <Route
               path="/personal"
-              element={
-                <PersonalWork
-                  navColor={"dark"}
-                  menuActive={menuActive}
-                  setMenuActive={setMenuActive}
-                  lightboxActive={lightboxActive}
-                  setLightboxActive={setLightboxActive}
-                />
-              }
+              element={<PersonalWork {...commonProps} />}
             />
-            <Route
-              path="/aboutMe"
-              element={
-                <AboutMe
-                  navColor={"dark"}
-                  menuActive={menuActive}
-                  setMenuActive={setMenuActive}
-                />
-              }
-            />
+            <Route path="/aboutMe" element={<AboutMe {...commonProps} />} />
           </Routes>
         </main>
         <Footer />
